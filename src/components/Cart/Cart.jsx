2faea781d1d8e4f89a5cc15d489fdd59380c5f64
Developer Junaid <qjunaid623@gmail.com>
2021-12-12 22:12:42 +0500

    empty card handling

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -17,7 +17,14 @@ export const Cart = () => {
     <div className="flex justify-center my-6">
       <div className="flex flex-col w-full p-8 text-gray-800 bg-white shadow-lg pin-r pin-y md:w-4/5 lg:w-4/5">
         <div className="flex-1">
-          <CartProducts products={cart} />
+          {cart.length > 0 ? (
+            <CartProducts products={cart} />
+          ) : (
+            <div className="bg-gray-200 flex justify-center items-center text-center p-6 rounded-md">
+              <span className=" text-xl text-gray-800">Cart is Empty</span>
+            </div>
+          )}
+
           <hr className="pb-6 mt-6" />
           <div className="my-4 mt-6 -mx-2 lg:flex">
             <div className="lg:px-2 lg:w-1/2">
