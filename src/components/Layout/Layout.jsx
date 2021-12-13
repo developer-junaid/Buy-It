936428ef7876ed0ec3d6747cd76935c59b12e870
Developer Junaid <qjunaid623@gmail.com>
2021-12-13 13:08:49 +0500

    added products to product section

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -10,31 +10,34 @@ import { Nav } from "./Nav"
 
 // Context
 import { CartContextProvider } from "context/cartContext"
+import { ProductsContextProvider } from "context/productsContext"
 
 // Layout
 export const Layout = ({ children }) => {
   return (
-    <CartContextProvider>
-      <div className="container mx-auto p-5 max-w-screen-xl flex flex-col justify-between min-h-screen">
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Buy-it</title>
-          <link rel="icon" href={titleIcon} />
-          <link rel="canonical" href="#" />
-          <link
-            rel="shortcut icon"
-            href="assets/cart.svg"
-            type="image/x-icon"
-          />
-          <meta
-            name="description"
-            content="Ecommerce Application template, built using Gatsby, Stripe and Tailwindcss"
-          />
-        </Helmet>
-        <Nav />
-        {children}
-        <Footer />
-      </div>
-    </CartContextProvider>
+    <ProductsContextProvider>
+      <CartContextProvider>
+        <div className="container mx-auto p-5 max-w-screen-xl flex flex-col justify-between min-h-screen">
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>Buy-it</title>
+            <link rel="icon" href={titleIcon} />
+            <link rel="canonical" href="#" />
+            <link
+              rel="shortcut icon"
+              href="assets/cart.svg"
+              type="image/x-icon"
+            />
+            <meta
+              name="description"
+              content="Ecommerce Application template, built using Gatsby, Stripe and Tailwindcss"
+            />
+          </Helmet>
+          <Nav />
+          {children}
+          <Footer />
+        </div>
+      </CartContextProvider>
+    </ProductsContextProvider>
   )
 }
---
+++ src/context/productsContext.jsx
@@ -0,0 +1,69 @@
+import React, { useState, createContext, useEffect } from "react"
+
+// GraphQl
+import { useStaticQuery, graphql } from "gatsby"
+
+export const ProductsStateContext = createContext(null)
+
+export const ProductsContextProvider = ({ children }) => {
+  const [products, setProducts] = useState([])
+
+  // Get Data
+  let allData = useStaticQuery(
+    graphql`
+      query GetProductsPrices {
+        prices: allStripePrice(
+          filter: { active: { eq: true }, product: { active: { eq: true } } }
+        ) {
+          edges {
+            node {
+              id
+              currency
+              product {
+                id
+                description
+                images
+                name
+                metadata {
+                  tag
+                }
+              }
+              unit_amount_decimal
+            }
+          }
+        }
+      }
+    `
+  )
+
+  // Optimize Data
+  allData = allData.prices.edges
+  const productsData = []
+
+  allData.map(({ node: price }) => {
+    const product = price.product
+
+    productsData.push({
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      image: product.images[0],
+      price: (parseFloat(price.unit_amount_decimal) / 100).toFixed(2),
+      currency: price.currency,
+      priceId: price.id,
+      tag: product.metadata.tag,
+    })
+
+    return null
+  })
+
+  useEffect(() => {
+    setProducts(productsData)
+  }, [])
+
+  return (
+    <ProductsStateContext.Provider value={{ products, setProducts }}>
+      {children}
+    </ProductsStateContext.Provider>
+  )
+}
